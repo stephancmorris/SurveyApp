b75fab646362e192058f700fc0dca2cbc8271c87
Stephan Morris <>
2018-09-08 12:55:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+//Running express Server
+//Setup configuration that will listen to request and route them to different route handlers
+
+app.get('/', (req, res) => {
+    //Calling a Route Handler
+    // '/' = localhost:5000/
+
+    res.send({
+        hi: 'there'
+    });
+}); //Route handler with express
+
+const PORT = process.env.PORT || 5000;
+// look at the underline environment to see what PORT needs to be used
+// environment var = varables that are set in the underline runtime that node is running on top of
+app.listen(PORT);
+//Express tells Node to listen for port 5000
+
+
+
+//localhost:5000
