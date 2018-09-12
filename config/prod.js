a315f0a12e6065d9ae3e4dfa35b70899a224b06c
Stephan Morris <>
2018-09-11 21:33:25 -0400

    tweak

--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,7 @@
 //Production keys
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 };
-
-// googleClientID: '769156397863-otd848ffl8gl1id0nahva95krd779pqk.apps.googleusercontent.com',
-// googleClientSecret: 'CeCQ4Rs9G-1IB_4C0x1QqhJJ',
-// mongoURI: 'mongodb://scm:devpassword1@ds251632.mlab.com:51632/emaily-dev',
-//     cookieKey: 'alksjdfh23532qweiury431534zmrewg324'
