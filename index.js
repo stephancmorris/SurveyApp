8408e5d73bacc6740a33580516f00a717e4a0762
Stephan Morris <>
2018-09-11 20:47:38 -0400

    finshed auth flow

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys')
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 //Running express Server
 //Setup configuration that will listen to request and route them to different route handlers
 
-app.get('/', (req, res) => {
-    //Calling a Route Handler
-    // '/' = localhost:5000/
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
 
-    res.send({
-        bye: 'buddy'
-    });
-}); //Route handler with express
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+// app.get('/', (req, res) => {
+//     //Calling a Route Handler
+//     // '/' = localhost:5000/
+
+//     res.send({
+//         bye: 'buddy'
+//     });
+// }); //Route handler with express
 
 const PORT = process.env.PORT || 5000;
 // look at the underline environment to see what PORT needs to be used
 // environment var = varables that are set in the underline runtime that node is running on top of
 app.listen(PORT);
-//Express tells Node to listen for port 5000
-
-
-
 //localhost:5000
---
+++ services/passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+    //ID created from Mongo
+}); //mongoose model instance => user ID
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        });
+}); //user ID into a mongoose model instance
+
+
+passport.use(new GoogleStrategy({
+        //Config Options
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, //2nd Arguement = Callback
+    (accessToken, refreshToken, profile, done) => {
+        User.findOne({ //MongoDB Query
+            googleId: profile.id
+        }).then((existingUser) => {
+            if (existingUser) {
+                //we already have a record with the profile ID
+                done(null, existingUser);
+            } else {
+                new User({
+                    googleId: profile.id //still need to add to the MongoDB
+                    //.save saves it to the mondo
+                }).save().then(user => done(null, user));
+            }
+        })
+    }));
